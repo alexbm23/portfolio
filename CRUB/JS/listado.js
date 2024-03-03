82ecb39a1422cc54768fc1b91f2d1cf67f881b1f
Alejandro Barnestein <118524728+alexbm23@users.noreply.github.com>
2024-03-03 19:00:29 +0100

    Delete CRUB directory

--- CRUB/JS/listado.js
+++
@@ -1,9 +0,0 @@
-/******************************************************/
-/**** SCRIPTS DE LISTADO.HTML*******************/
-/**** Autor: Alejandro Barnestein Martos **************/
-/**** Versión 2: Febrero 2024, Lisado validado*/
-/*******************************************************/
-
-function confirmar(){
-    confirm("¿Está seguro de que quiere eliminar este empleado?");
-}
