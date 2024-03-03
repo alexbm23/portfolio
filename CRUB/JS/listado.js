dba7bfd72b0316c7c11c4b4479fc7fe72f396522
Alejandro Barnestein <118524728+alexbm23@users.noreply.github.com>
2024-03-03 19:01:03 +0100

    Add files via upload

---
+++ CRUB/JS/listado.js
@@ -0,0 +1,9 @@
+/******************************************************/
+/**** SCRIPTS DE LISTADO.HTML*******************/
+/**** Autor: Alejandro Barnestein Martos **************/
+/**** Versión 2: Febrero 2024, Lisado validado*/
+/*******************************************************/
+
+function confirmar(){
+    confirm("¿Está seguro de que quiere eliminar este empleado?");
+}
